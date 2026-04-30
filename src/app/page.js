c1c7661624d09089d895483a34f006e832075015
fuad-dev-portfolio <abdullahalfuad.dev@gmail.com>
2026-04-30 20:54:48 +0600

    feat: top selling product page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Showcase from "@/components/Showcase.jsx";
 import NewArraivals from "@/components/New-Arraivals.jsx";
 import AllProducts from "@/components/AllProducts.jsx";
+import TopSelling from "@/components/TopSelling.jsx";
 
 export default function Home() {
   return (
     <div>
       <Showcase />
       <NewArraivals />
+      <TopSelling />
       <AllProducts />
     </div>
   );
