85e42841797e9614fd46cfc710799beb044320a2
fuad-dev-portfolio <abdullahalfuad.dev@gmail.com>
2026-04-30 21:44:03 +0600

    feat: deploying admin header page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function AdminDashboard() {
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center h-screen">
             <Link href="/">
                 <Image src="/logo.png" alt="Dashboard" width={200} height={200} />
             </Link>
