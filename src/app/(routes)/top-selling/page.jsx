d528c71b151030a04581ab3d73824d124d729f87
fuad-dev-portfolio <abdullahalfuad.dev@gmail.com>
2026-04-30 20:51:39 +0600

    feat: top selling product page

---
+++ src/app/(routes)/top-selling/page.jsx
@@ -0,0 +1,13 @@
+import TopSelling from "@/components/TopSelling";
+
+export default function TopSellingPage() {
+    return (
+        <div className="min-h-screen bg-gray-50 py-8">
+            <div className="max-w-7xl mx-auto px-4">
+                <div className="bg-white rounded-lg shadow-sm p-6">
+                    <TopSelling />
+                </div>
+            </div>
+        </div>
+    );
+}
